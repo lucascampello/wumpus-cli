2c023855c00cbd1ceab77748eb12a66333961fc6
Lucas Campello <lucas.campello@gmail.com>
2021-06-09 11:21:18 -0300

    Delete teste.php

--- teste.php
+++
@@ -1,9 +0,0 @@
-<?php
-// Constantes que serão utilizadas em todo o código
-include("define.php");
-
-// Define o tempo máximo de execução do Script (MAX_EXEC² segundos)
-set_time_limit(MAX_EXEC * MAX_EXEC);
-require(DIR_CLASS."Autoloader.class.php");
-
-$objJogador = new Jogador();
